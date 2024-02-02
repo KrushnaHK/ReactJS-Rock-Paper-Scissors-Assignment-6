c925b3cae209ad7a2039d8d121de806dfbb14b39
KrushnaHK <krushnahandibag02@gmail.com.com>
2024-02-02 05:17:56 +0000

    Rock Paper Scissors

---
+++ src/GameResultView/styledComponents.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components'
+
+export const ShowResultContainer = styled.div`
+  width: 100%;
+`
+export const ResultIMagesContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`
+
+export const ResultTextImgContainer = styled.div``
+
+export const ResultText = styled.p`
+  text-align: center;
+  font-weight: bold;
+  font-size: 20px;
+`
+
+export const ResultImageItem = styled.img`
+  width: 150px;
+  height: 150px;
+`
+
+export const ResultButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`
+
+export const PlayAgainButton = styled.button`
+  border: 0;
+  background-color: #ffffff;
+  color: #000000;
+  font-family: 'Roboto';
+  font-size: 15px;
+  padding: 10px;
+  font-weight: bold;
+  border-radius: 10px;
+  cursor: pointer;
+`
